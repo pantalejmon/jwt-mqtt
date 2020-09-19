05a109659b03b48abad57874a1a4c6a0931802a1
Jan Jakubik <janekjak@gmail.com>
2020-09-19 10:59:48 +0200

    clean

--- src/clientToken.ts
+++
@@ -1,7 +0,0 @@
-import {Client} from "aedes";
-
-export interface ClientToken {
-    client: Client;
-    token: string;
-    topics: Array<string>;
-}
--- src/keycloakConfig.ts
+++
@@ -1,8 +0,0 @@
-export interface KeycloakConfig {
-    "realm": string;
-    "auth-server-url": string;
-    "client_id": string;
-    "client_secret": string;
-    "username": string;
-    "password": string;
-}
